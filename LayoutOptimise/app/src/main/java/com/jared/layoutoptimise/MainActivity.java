a49e1eca848d1de4e41138f58ae47ee810854d70
eastmoon <chenjianneng1117@gmail.com>
2016-12-31 21:39:48 +0800

    add LayoutOptimise test Signed-off-by: eastmoon <chenjianneng1117@gmail.com>

---
+++ LayoutOptimise/app/src/main/java/com/jared/layoutoptimise/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jared.layoutoptimise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewStub;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewStub viewStub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_test_constaint_layout);
+//        setContentView(R.layout.activity_main);
+//        viewStub = (ViewStub) findViewById(R.id.vs_test);
+//        findViewById(R.id.btn_view_show).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                viewStub.setVisibility(View.VISIBLE);
+//            }
+//        });
+//        findViewById(R.id.btn_view_hide).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                viewStub.setVisibility(View.GONE);
+//            }
+//        });
+    }
+}
